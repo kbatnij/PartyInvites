0ea1b58af3e885d73ea9702c8170c13dbad88ea2
Khaldon Batnij <batnij.k@gmail.com>
2017-08-13 15:22:43 -0500

    RSVP Website done.
    WebMail settings needs to be set to the correct settings to send emails to people who RSVP.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Email reponse to respective parties
+                return View("Thanks", guestResponse);
+            }
+            // Validation error
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="Please enter name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Please enter email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage ="Please enter a valid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Please enter phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage ="Please choose whether you'll attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
